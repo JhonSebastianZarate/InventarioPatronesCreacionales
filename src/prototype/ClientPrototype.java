2f792b1bbe7b216f7a29c623bdb8bc2204a158f1
Sebastian Zarate <jhon-zarate@upc.edu.co>
2025-09-13 12:13:08 -0500

    Agregado cliente ClientPrototype para probar el patrón Prototype

---
+++ src/prototype/ClientPrototype.java
@@ -0,0 +1,20 @@
+package prototype;
+
+public class ClientPrototype {
+    public static void main(String[] args) {
+        // Crear prototipo 1
+        ConcretePrototype prototype1 = new ConcretePrototype("Ejemplo1");
+        ConcretePrototype clone1 = (ConcretePrototype) prototype1.clone();
+
+        // Crear prototipo 2
+        ConcretePrototype2 prototype2 = new ConcretePrototype2("Rojo");
+        ConcretePrototype2 clone2 = (ConcretePrototype2) prototype2.clone();
+
+        // Mostrar resultados
+        System.out.println("Original 1: " + prototype1);
+        System.out.println("Clon 1: " + clone1);
+
+        System.out.println("Original 2: " + prototype2);
+        System.out.println("Clon 2: " + clone2);
+    }
+}
